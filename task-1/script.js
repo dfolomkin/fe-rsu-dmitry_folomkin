1f3d1dd4cb0a55b62890eb022f38ff9021b242b1
d.folomkin <d.folomkin@yandex.ru>
2017-04-20 00:00:13 +0300

    do task-1 abd task-2

---
+++ task-1/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function consoleRec(arr) {
+    if (arr.length > 0) {
+        console.log(arr.shift());
+        consoleRec(arr);
+    }
+}
+
+consoleRec(['я', 'умею', 'писать', 'рекурсивные', 'функции']);
---
+++ task-2/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function isAllTrue(source, filterFn) {
+    for (var i = 0; i < source.length; i++) {
+        if (!filterFn(source[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function isNumber(val) {
+    return typeof val === 'number';
+}
+
+function isBoolean(val) {
+    return typeof val === 'boolean';
+}
+
+function isString(val) {
+    return typeof val === 'string';
+}
+
+function isObject(val) {
+    return typeof val === 'object';
+}
+
+function isFunction(val) {
+    return typeof val === 'function';
+}
+
+function isUndefined(val) {
+    return typeof val === 'undefined';
+}
+
+var allNumbers = [1, 2, 4, 5, 6, 7, 8];
+var someNumbers = [1, 2, 'привет', 4, 5, 'ololo', 6, 7, 8];
+var noNumbers = ['это', 'массив', 'без', 'чисел'];
+
+console.log(isAllTrue(allNumbers, isNumber)); //вернет true
+console.log(isAllTrue(someNumbers, isNumber)); //вернет false
+console.log(isAllTrue(noNumbers, isNumber)); //вернет false
+
+console.log(isAllTrue(noNumbers, isString)); //вернет true
