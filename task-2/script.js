813f49f81e71b6d6761ec9b1d584ea24a5e9d398
d.folomkin <d.folomkin@yandex.ru>
2017-04-20 01:23:21 +0300

    fix task-1 and task-2

--- task-2/script.js
+++ task-2/script.js
@@ -10,32 +10,32 @@ function isAllTrue(source, filterFn) {
 }
 
 function isNumber(val) {
-    return typeof val === 'number';
+    return typeof val === "number";
 }
 
 function isBoolean(val) {
-    return typeof val === 'boolean';
+    return typeof val === "boolean";
 }
 
 function isString(val) {
-    return typeof val === 'string';
+    return typeof val === "string";
 }
 
 function isObject(val) {
-    return typeof val === 'object';
+    return typeof val === "object";
 }
 
 function isFunction(val) {
-    return typeof val === 'function';
+    return typeof val === "function";
 }
 
 function isUndefined(val) {
-    return typeof val === 'undefined';
+    return typeof val === "undefined";
 }
 
 var allNumbers = [1, 2, 4, 5, 6, 7, 8];
-var someNumbers = [1, 2, 'привет', 4, 5, 'ololo', 6, 7, 8];
-var noNumbers = ['это', 'массив', 'без', 'чисел'];
+var someNumbers = [1, 2, "привет", 4, 5, "ololo", 6, 7, 8];
+var noNumbers = ["это", "массив", "без", "чисел"];
 
 console.log(isAllTrue(allNumbers, isNumber)); //вернет true
 console.log(isAllTrue(someNumbers, isNumber)); //вернет false
