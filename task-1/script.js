202e256881557044f4ef3682b1c2875284248524
d.folomkin <d.folomkin@yandex.ru>
2017-05-01 23:27:25 +0300

    do task-1 and task-2

--- task-1/script.js
+++ task-1/script.js
@@ -1,16 +1,18 @@
-"use strict";
-
-document.getElementById("button1").addEventListener("click", function() { prepend("#target","#source") });
-
 function prepend(targetSelector, sourceSelector) {
+    "use strict";
     var sourceElement = document.querySelector(sourceSelector);
     var targetElement = document.querySelector(targetSelector);
     var prevFirstChild = targetElement.querySelector("*");
 
-    if (prevFirstChild != null) {
+    if (prevFirstChild !== null) {
         targetElement.insertBefore(sourceElement, prevFirstChild);
     } else {
         targetElement.appendChild(sourceElement);
     }
 }
 
+if (typeof document !== 'undefined') {
+    document.getElementById("button1").addEventListener("click", function () {
+        prepend("#target", "#source");
+    });
+}
---
+++ task-2/script.js
@@ -0,0 +1,20 @@
+function deleteTextNodes(elem) {
+    "use strict";
+    var i = 0;
+    while (elem.childNodes[i] !== undefined) {
+        if (elem.childNodes[i].nodeType == 3) {
+            elem.removeChild(elem.childNodes[i]);
+        } else if (elem.childNodes[i].nodeType == 1) {
+            deleteTextNodes(elem.childNodes[i]);
+            i++;
+        } else {
+            i++;
+        }
+    }
+}
+
+if (typeof document !== 'undefined') {
+    document.getElementById("button1").addEventListener("click", function () {
+        deleteTextNodes(document.body);
+    });
+}
