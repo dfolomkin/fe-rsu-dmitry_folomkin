737b52cd79d3abfec82d3dd21ac3bbc492b02f08
Тестируемый(ая) <t@local.rsu.edu.ru>
2017-05-04 20:09:48 +0300

    fix init array changing

--- task-1/script.js
+++ task-1/script.js
@@ -1,10 +1,22 @@
 "use strict";
 
 function consoleRec(arr) {
-    if (arr.length > 0) {
-        console.log(arr.shift());
-        consoleRec(arr);
+    //clone instead of reference
+    var arr2 = [];
+    for (var i = 0; i < arr.length; i++) {
+        arr2[i] = arr[i];
+    }
+
+    if (arr2.length > 0) {
+        console.log(arr2.shift());
+        consoleRec(arr2);
     }
 }
 
-consoleRec(["я", "умею", "писать", "рекурсивные", "функции"]);
+var initArr = ["я", "умею", "писать", "рекурсивные", "функции"];
+//check initial array
+console.log(initArr);
+//execute cutting function
+consoleRec(initArr);
+//initial array hasn't change
+console.log(initArr);
